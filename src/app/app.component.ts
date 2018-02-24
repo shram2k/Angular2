9c1f895bb8029b3d40e85bdcfdcb29295e7fd7fa
shram2k <shram2k@gmail.com>
2018-02-24 21:43:47 +0530

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
-  //styles: [require('./app.component.css'), require('../../node_modules/bootstrap/dist/css/bootstrap.min.css')]
+  templateUrl: './app.component.html',
+  providers: [ProductService]
+  // styles: [require('./app.component.css'), require('../../node_modules/bootstrap/dist/css/bootstrap.min.css')]
   //styles: ['../node_modules/bootstrap/dist/css/bootstrap.min.css']
 //  styleUrls: ['./app.component.css','../node_modules/bootstrap/dist/css/bootstrap.min.css']
 })
 export class AppComponent {
- 
+
 }
