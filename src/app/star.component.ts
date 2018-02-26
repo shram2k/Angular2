61b634c28d9cc6fa58d063a88484abcd6e9bfe1d
Ram Kumar Sharma <Ram.Sharma@rsystems.com>
2018-02-26 13:14:04 +0530

    merged

--- src/app/star.component.ts
+++ src/app/star.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnChanges, Input } from "@angular/core";
-
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector:'pm-star',
@@ -8,18 +7,12 @@ import { Component, OnChanges, Input } from "@angular/core";
 })
 export class StarComponent implements OnChanges {
     @Input() rating: number = 4;
-    @Output() notify:
+    @Output() ratingClicked: EventEmitter<string>= new EventEmitter<string>();
     starWidth: number;
     ngOnChanges(): void {
         this.starWidth = this.rating * 86/5;
     }
-<<<<<<< HEAD
     onClick(): void {
-        console.log('The rating ' + this.rating + ' was clicked');
-=======
-    onClick() {
-        console.log('clicked');
         this.ratingClicked.emit('The rating ' + this.rating + ' clicked!!');
->>>>>>> af9abddc274cf8a30306b750897a0bf58e58fdff
     }
 }
